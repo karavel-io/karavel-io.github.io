1d765cf8ddd25b14b2a00d0edf9388d7025a3e5c
WilliamRedPanda <william.lacalamita@gmail.com>
2021-10-06 15:58:55 +0200

    add scroll on menu link

--- themes/karavel/assets/javascripts/components/off-canvas-menu.js
+++ themes/karavel/assets/javascripts/components/off-canvas-menu.js
@@ -11,16 +11,27 @@ class OffCanvasMenu {
   init() {
     this.bindEvents();
   }
-  openMenu() {
+  toggleMenu() {
     const element = document.getElementById("off-canvas-menu");
     element.classList.toggle("menu-open");
     const menuIcon = document.getElementById("toggle-menu");
     menuIcon.classList.toggle("open");
   }
+  closeMenu() {
+    const element = document.getElementById("off-canvas-menu");
+    element.classList.remove("menu-open");
+    const menuIcon = document.getElementById("toggle-menu");
+    menuIcon.classList.remove("open");
+  }
 
   bindEvents() {
     const element = document.getElementById("toggle-menu");
-    element.addEventListener("click", this.openMenu);
+    element.addEventListener("click", this.toggleMenu);
+
+    const links = document.getElementsByClassName("off-canvas-menu-link");
+    Object.keys(links).forEach((key) => {
+      links[key].addEventListener("click", this.closeMenu);
+    });
   }
 }
 
