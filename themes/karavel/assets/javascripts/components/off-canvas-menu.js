845cc56106c650a68bf54d64c3180a27c112280f
WilliamRedPanda <william.lacalamita@gmail.com>
2021-10-06 16:45:37 +0200

    add right swipe

--- themes/karavel/assets/javascripts/components/off-canvas-menu.js
+++ themes/karavel/assets/javascripts/components/off-canvas-menu.js
@@ -40,59 +40,31 @@ class OffCanvasMenu {
       links[key].addEventListener("click", this.closeMenu);
     });
 
-    document.addEventListener("touchstart", this.handleTouchStart);
-    document.addEventListener("touchmove", this.leftSwipe);
+    document.addEventListener("touchstart", (event) =>
+      this.handleTouchStart(event)
+    );
+    document.addEventListener("touchmove", (event) => this.handleSwipe(event));
   }
 
   handleTouchStart(event) {
     const firstTouch = event.touches[0];
     this.xDown = firstTouch.clientX;
-    console.log("touchstart", this.xDown);
   }
 
-  leftSwipe(event) {
+  handleSwipe(event) {
     if (!this.xDown) {
       return;
     }
     const xUp = event.touches[0].clientX;
     const swipeDifference = this.xDown - xUp;
-    console.log("youche move", swipeDifference);
+
     if (swipeDifference < 0) {
-      const element = document.getElementById("off-canvas-menu");
-      element.classList.add("menu-open");
-      const menuIcon = document.getElementById("toggle-menu");
-      menuIcon.classList.add("open");
+      this.openMenu();
+    } else {
+      this.closeMenu();
     }
     this.xDown = null;
   }
-  //   handleTouchMove(evt) {
-  //     if ( ! xDown || ! yDown ) {
-  //         return;
-  //     }
-
-  //     var xUp = evt.touches[0].clientX;
-  //     var yUp = evt.touches[0].clientY;
-
-  //     var xDiff = xDown - xUp;
-  //     var yDiff = yDown - yUp;
-
-  //     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-  //         if ( xDiff > 0 ) {
-  //             /* right swipe */
-  //         } else {
-  //             /* left swipe */
-  //         }
-  //     } else {
-  //         if ( yDiff > 0 ) {
-  //             /* down swipe */
-  //         } else {
-  //             /* up swipe */
-  //         }
-  //     }
-  //     /* reset values */
-  //     xDown = null;
-  //     yDown = null;
-  // };
 }
 
 export { OffCanvasMenu };
