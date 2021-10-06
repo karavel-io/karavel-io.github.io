4a3663b8a2485af2c7b7986675a4fe34e93b430e
WilliamLacalamitaNeo <william.lacalamita@mikamai.com>
2021-10-06 11:29:47 +0200

    feat: initial blockout of the page

--- themes/karavel/assets/javascripts/components/off-canvas-menu.js
+++ themes/karavel/assets/javascripts/components/off-canvas-menu.js
@@ -1,6 +1,6 @@
 class OffCanvasMenu {
   constructor(selector) {
-    document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener("DOMContentLoaded", () => {
       if (document.querySelectorAll(selector).length > 0) {
         this.selector = selector;
         this.init();
@@ -11,8 +11,14 @@ class OffCanvasMenu {
   init() {
     this.bindEvents();
   }
+  openMenu() {
+    const element = document.getElementById("off-canvas-menu");
+    element.classList.add("menu-open");
+  }
 
   bindEvents() {
+    const element = document.getElementById("menu-button");
+    element.addEventListener("click", this.openMenu);
   }
 }
 
